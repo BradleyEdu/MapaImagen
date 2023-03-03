d506d8eeecbb1b94dfd1e18c2935ab7c4239e37c
Eduardo Leyva Bradley <eduardolb@inavant.com.mx>
2023-03-03 11:22:59 -0600

    commit inicial

---
+++ src/webparts/mapaImagen/components/IMapaImagenProps.ts
@@ -0,0 +1,7 @@
+export interface IMapaImagenProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/mapaImagen/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IMapaImagenWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'MapaImagenWebPartStrings' {
+  const strings: IMapaImagenWebPartStrings;
+  export = strings;
+}
