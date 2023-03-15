af75b8b994dc38d07c6c4018787b7f954d51de66
Eduardo Leyva Bradley <eduardolb@inavant.com.mx>
2023-03-15 12:54:56 -0600

    Cambio  de funcion a Clase

--- src/webparts/mapaImagen/components/Mapa.tsx
+++ src/webparts/mapaImagen/components/Mapa.tsx
@@ -2,26 +2,29 @@ import { ImageMap } from '@qiuz/react-image-map';
 import { IMapaProps } from './IMapaProps';
 import * as React from 'react';
 
+class Mapa extends React.Component<IMapaProps> {
 
-function Mapa(props: IMapaProps){
-	const {imagen, mapArea, buscarInfo} = props;
+  onMapClick = (area:any, index:number) => {
+    const { mapArea, buscarInfo } = this.props;
+    const tip = `click map${index + 1}`;
+    console.log(`Click en ${tip}  ${area}`);
+    buscarInfo(mapArea[index].id);
+  }
 
-	const onMapClick = (area:any, index:number) => {
-		const tip = `click map${index + 1}`;
-		console.log(`Click en ${tip}  ${area}`);
-		buscarInfo(mapArea[index].id);
-	}
+  render() {
+    const { imagen, mapArea } = this.props;
 
-	return (
-		<div>
-			<ImageMap
-				className="usage-map"
-				src={imagen}
-				map={mapArea}
-				onMapClick={onMapClick}
-			/>
-		</div>
-	)
+    return (
+      <div>
+        <ImageMap
+          className="usage-map"
+          src={imagen}
+          map={mapArea}
+          onMapClick={this.onMapClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default Mapa;
